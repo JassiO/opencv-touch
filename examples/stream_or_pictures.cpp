83e7162b414c453a74a9b45cf3d743bde40e40b5
Jasmin Odenwald <kuxe9212@apollo.(none)>
2014-06-05 14:35:52 +0200

    add buggy mser algorithm

--- examples/stream_or_pictures.cpp
+++ examples/stream_or_pictures.cpp
@@ -5,9 +5,17 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/background_segm.hpp>
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////																							/////
+/////	mser geht zur zeit gar nicht mehr; an sich läuft alles zwar, 							/////
+///// 	der algorithmus ist aber rauskommentiert, da es sonst nur zu Fehlern kommt				/////
+/////																							/////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 using namespace cv;
 
-int SYSTEM_INPUT = 1; // use a picture; change it to 1 to use the stream
+int SYSTEM_INPUT = 0; // uses a picture; change it to 1 to use the stream
+int ALGORITHM = 2; // uses only contours; change it to 1 to use MSER or to two to use both
 
 baumer::BCamera* g_cam = 0;
 baumer::BSystem* g_system  = 0;
@@ -15,18 +23,29 @@ baumer::BSystem* g_system  = 0;
 int width = 640;
 int height = 480;
 
-
-Mat frameMat;
+Mat frameMat, back, background, MaskMOG, img0, ellipses, gray;
 char key;
-Mat back;
-Mat background;
 Ptr<BackgroundSubtractor> pMOG;
-Mat MaskMOG;
+vector<vector<Point> > contours;
 
+static const Vec3b bcolors[] = {
+    Vec3b(0,0,255),
+    Vec3b(0,128,255),
+    Vec3b(0,255,255),
+    Vec3b(0,255,0),
+    Vec3b(255,128,0),
+    Vec3b(255,255,0),
+    Vec3b(255,0,0),
+    Vec3b(255,0,255),
+    Vec3b(255,255,255)
+};
+
+// callbacks
 void cleanup();
 void init_camera();
 void open_stream(int width, int height, Ptr<BackgroundSubtractor> pMOG);
 void get_contours(Mat img_cont);
+void mser_algo(Mat temp_img);
 
 int main() {
 
@@ -45,8 +64,7 @@ int main() {
 	}
 	
 	////////// PICTURE //////////
-	else {
-		namedWindow("Image", CV_WINDOW_AUTOSIZE);
+	else if (SYSTEM_INPUT == 0) {
 		namedWindow("Contour", CV_WINDOW_AUTOSIZE);
 
 		// load image
@@ -57,13 +75,25 @@ int main() {
 			return 0;
 		}
 
-		// show loaded image
-		imshow("Image", img);
-
-		get_contours(img);
+		if (ALGORITHM == 0) {
+			    	get_contours(img);
+			    }
+		else if (ALGORITHM == 1) {
+			mser_algo(img);
+		}
+		else if (ALGORITHM == 2) {
+			get_contours(img);
+			mser_algo(img);
+		}
+		else {
+			std::cout << "Error: invalide algorithm number" << std::endl;
+		}
 
 		// wait until any key is pressed
 		cvWaitKey(0);
+	}
+	else {
+		std::cout << "Error: invalide system input number" << std::endl;
 	}
 
 	return 0;
@@ -117,7 +147,19 @@ void open_stream(int width, int height, Ptr<BackgroundSubtractor> pMOG) {
 			    	frameMat = Mat(frame);
 			    }
 
-				get_contours(frameMat);
+			    if (ALGORITHM == 0) {
+			    	get_contours(frameMat);
+			    }
+				else if (ALGORITHM == 1) {
+					mser_algo(frameMat);
+				}
+				else if (ALGORITHM == 2) {
+					get_contours(frameMat);
+					mser_algo(frameMat);
+				}
+				else {
+					std::cout << "Error: invalide algorithm number" << std::endl;
+				}
 
 		        key = cvWaitKey(10); // throws a segmentation fault (?)
 	    	}
@@ -127,8 +169,8 @@ void open_stream(int width, int height, Ptr<BackgroundSubtractor> pMOG) {
 
 void get_contours(Mat img_cont) {
 	
-	// calculate contours
 	Mat img2;
+	
 	if (SYSTEM_INPUT == 0) {
 		cvtColor(img_cont, img_cont, CV_BGR2GRAY);
 		threshold(img_cont, img2, 65, 255, CV_THRESH_BINARY);
@@ -147,10 +189,49 @@ void get_contours(Mat img_cont) {
 	for( size_t k = 0; k < contours0.size(); k++ )
 		approxPolyDP(Mat(contours0[k]), contours[k], 3, true);
 
-	// draw contours
 	Mat cnt_img = Mat::zeros(img2.rows, img2.cols, CV_8UC3);
+	if (SYSTEM_INPUT == 0) {
+		img_cont.copyTo(cnt_img); // when it is used with the stream. it shows the same image as the mser algorithm
+	}
 	drawContours(cnt_img, contours, -1, Scalar(128,255,255));
 
-	// show contours
 	imshow("Contour", cnt_img);
+	
+
+}
+
+void mser_algo(Mat temp_img)  {
+
+	//cvtColor(temp_img, gray, COLOR_BGR2GRAY);
+
+	if (SYSTEM_INPUT == 0) {
+		cvtColor(temp_img, temp_img, CV_BGR2GRAY);
+	}
+
+	cvtColor(temp_img, img0, COLOR_GRAY2BGR);
+	img0.copyTo(ellipses);
+
+	//MSER ms;
+	//ms(gray, contours, Mat()); 			//do not work
+
+	if (contours.size() <= 0) {
+		std::cout << "Error: no contours extracted" << std::endl; // ERROR ???
+	}
+
+	for( int i = (int)contours.size()-1; i >= 0; i-- ) {
+		const vector<Point>& r = contours[i];
+		for ( int j = 0; j < (int)r.size(); j++ ) {
+		    Point pt = r[j];
+		    img0.at<Vec3b>(pt) = bcolors[i%9];
+		}
+
+		RotatedRect box = fitEllipse( r ); // maybe try cvfitellipse2
+
+		box.angle=(float)CV_PI/2-box.angle;
+		ellipse( ellipses, box, Scalar(196,255,255), 2 );
+	}
+
+	imshow( "response", img0);
+	imshow( "ellipses", ellipses);
+
 }
